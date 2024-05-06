29cf030f09033062f3bca7fd189a9b4b787c5160
kangsan lee <136150765+Ah-Kang@users.noreply.github.com>
2024-05-06 16:52:09 +0900

    Add files via upload
    추가기능: 공지사항, 자료실, 학생통계

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,7 +9,7 @@ const mock = new MockAdapter(axiosInstance);
 
 // 공지사항 API 경로와 가짜 데이터 설정
 mock.onGet("https://api.yourdomain.com/notices").reply(200, [
-  { id: 1, title: '첫 번째 공지', content: '이것은 공지사항 예시입니다. 아무말이나 길게 써야하기 때문에 아무말이나 짓껄이겠습니다.오늘 날씨는 너무 좋고 밖으로 나가 오토바이를 타지 않는다면 범죄입니다. 이상.', date: '2024-04-18' },
+  { id: 1, title: '첫 번째 공지', content: '이것은 공지사항 예시입니다. 아무말이나 길게 써야하기 때문에 아무말이나 짓껄이겠습니다.오늘 날씨는 너무 좋고 밖으로 나가 농구를 하면 좋을듯 합니다. 이상.', date: '2024-04-18' },
   { id: 2, title: '두 번째 공지', content: '세부 내용을 여기에 포함합니다.', date: '2024-04-19' },
 ]);
 
